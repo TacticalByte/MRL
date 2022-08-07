cd5e5c21923769bf39b4b44c25283a8d8596ed8f
Marcos Barrera <mmbarrerae@gmail.com>
2022-08-07 12:17:13 -0600

    removed test endpoint. Swagger configuration moved to a proper object

--- backend/index.js
+++ backend/index.js
@@ -4,50 +4,20 @@ const expressJSDocSwagger= require('express-jsdoc-swagger');
 const server = express();
 
 dotenv.config();
-
+const SwaggerOptions = require('./services/SwaggerConfiguration');
+const DataAccess = require('./services/DataAccess');
 const RequestLogger = require('./middlewares/RequestLogger');
+
 const AccountsController = require('./controllers/AccountsController');
 const PlayerAccountController = require('./controllers/PlayerAccountController');
 const UserTypeController = require('./controllers/UserTypeController');
 
-const DataAccess = require('./services/DataAccess');
-
-const options = {
-    info: {
-      version: '1.0.0',
-      title: 'MRL Website API',
-      description: 'Base MRL Website API.',
-      license: {
-        name: 'MIT',
-      },
-    },
-    security: {
-      BasicAuth: {
-        type: 'http',
-        scheme: 'basic',
-      },
-    },
-    baseDir: __dirname,
-    filesPattern: './controllers/*.js',
-  };
-
-expressJSDocSwagger(server)(options);
+expressJSDocSwagger(server)(SwaggerOptions);
 
 server.use(RequestLogger);
-
 server.use('/accounts',AccountsController);
 server.use('/player',PlayerAccountController);
 server.use('/usertype',UserTypeController);
-
-server.get('/', (req,res) =>{
-  let params = {
-    Property1: 1,
-    Property2: 2
-  }
-  DataAccess.ExecuteStoredProcedure('BAD.SP_GET_ACCOUNT',{Email: 'mmbarrerae@gmail.com'});
-  // DataAccess.GetAccounts();
-  res.send('executed!');
-})
 
 server.listen(process.env.SERVER_PORT, () =>{
     console.log(`Server running at localhost, port: ${process.env.SERVER_PORT}`);
---
+++ backend/services/SwaggerConfiguration.js
@@ -0,0 +1,20 @@
+const options = {
+    info: {
+      version: '1.0.0',
+      title: 'MRL Website API',
+      description: 'Base MRL Website API.',
+      license: {
+        name: 'MIT',
+      },
+    },
+    security: {
+      BasicAuth: {
+        type: 'http',
+        scheme: 'basic',
+      },
+    },
+    baseDir: __dirname,
+    filesPattern: './controllers/*.js',
+  };
+
+  module.exports = options;
